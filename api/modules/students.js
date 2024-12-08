91ab802cc786afa0ac3e2cca3bd5e63108498a41
Rat <mezeninvalentin@yandex.ru>
2024-12-09 00:21:30 +0300

    onestudent1

--- api/modules/students.js
+++ api/modules/students.js
@@ -1,15 +1,14 @@
 const {MongoClient, ObjectId} = require("mongodb");
 const client = new MongoClient("mongodb+srv://mezeninvalentin:CwOBHe73GTUB2qTl@instructorcluster0.ztxpz.mongodb.net/?retryWrites=true&w=majority&appName=InstructorCluster0");
-const db = client.db("students")
+const collection = client.db("students").collection("B");
+// const collection = await db.
 
 
 module.exports.getAllStudentsCategoryB = async (req, res) => {
-    const collection = await db.collection("B");
     const result = await collection.find().toArray();
     res.send(result);
 }
 module.exports.addStudent = async (req, res) => {
-    const collection = db.collection("B");
     if (!req.body) return res.sendStatus(400);
     const result = await collection.insertOne(req.body);
     console.log('req.body', req.body)
@@ -19,10 +18,22 @@ module.exports.getOneStudentById = async (req, res) => {
     const id = req.params.id;
     const newId = new ObjectId(id)
     // получаем одного пользователя по id
-    const collection = db.collection("B");
     const student = await collection.findOne({'_id': newId})
     if(student) res.json(student);
     else res.json.sendStatus(404);
+}
+
+module.exports.editStudentGrades = async (req, res) => {
+    const id = req.params.id;
+    const newId = new ObjectId(id)
+    if (!req.body) return res.sendStatus(400);
+    // const oldTitle = req.body.oldTitle;
+
+    console.log('req.body', req.body)
+    // const newGrades = {req.body.exercise};
+    // обновляем данные
+    const result = await collection.findOneAndUpdate({_id: newId}, {$set: newGrades});
+    res.status(200).json({result})
 }
 // module.exports.getFilterOneCategory = async (req, res) => {
 //     console.log(req.query)
